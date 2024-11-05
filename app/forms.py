f4763756ad5783ad15f3053a841ee63fdf396e23
Avina <mauryavina20@gmail.com>
2024-11-04 20:58:33 -0500

    edit profile birthday home work favourite

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Profile, Station
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['birth_date', 'home_latitude', 'home_longitude', 'work_latitude', 'work_longitude', 'fav_station']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'fav_station': forms.Select(),  # Dropdown for stations
+        }
